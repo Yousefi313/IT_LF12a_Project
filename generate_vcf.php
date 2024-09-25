77ffb2594093c0498bcc7be4b09e4e980a4d7982
Hassan313 <hassanyousefioszimt@gmail.com>
2024-09-25 15:05:46 +0200

    first day of the project

---
+++ generate_vcf.php
@@ -0,0 +1,40 @@
+<?php
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $website = isset($_POST['website']) ? $_POST['website'] : '';
+
+    // Create a vCard string
+    $vcfData = "BEGIN:VCARD\r\n";
+    $vcfData .= "VERSION:3.0\r\n";
+    $vcfData .= "FN:{$firstName} {$lastName}\r\n";
+    $vcfData .= "N:{$lastName};{$firstName};;;\r\n";
+    $vcfData .= "TEL;TYPE=WORK,VOICE:{$phone}\r\n";
+    $vcfData .= "EMAIL;TYPE=PREF,INTERNET:{$email}\r\n";
+    $vcfData .= "ADR;TYPE=WORK:;;{$address};;;;\r\n";
+
+    // Add website if provided
+    if (!empty($website)) {
+        $vcfData .= "URL:{$website}\r\n";
+    }
+
+    $vcfData .= "END:VCARD\r\n";
+
+    // Set the filename
+    $fileName = "{$firstName}_{$lastName}.vcf";
+
+    // Set headers to force download of the VCF file
+    header('Content-Type: text/vcard');
+    header('Content-Disposition: attachment; filename="' . $fileName . '"');
+    header('Content-Length: ' . strlen($vcfData));
+
+    // Output the VCF data
+    echo $vcfData;
+    exit;
+}
+?>
